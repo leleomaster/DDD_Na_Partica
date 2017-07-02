486cb3161d40856f6a8d8760331c19523f6b0178
leleomaster <ljcamposjr@outlook.com>
2017-07-02 19:50:43 -0300

    Update UsuarioController.cs

--- DDD_Na_Partica.Web/Controllers/UsuarioController.cs
+++ DDD_Na_Partica.Web/Controllers/UsuarioController.cs
@@ -10,6 +10,7 @@ namespace DDD_Na_Partica.Web.Controllers
     // https://www.youtube.com/watch?v=ywWWhIJYoDY&list=PLKzcE63mS1V1zvjQViINZAaFliO0-NDIM&index=7
 
     /*
+    teste
      * Fazer depois Curso de TDD mais nUnit
      https://www.youtube.com/playlist?list=PLb2HQ45KP0WvzEKQ56AZ7j5-Gsay9yPOg
      */
